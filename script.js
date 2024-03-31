e65838915d8fba9d090f9b5b8f6773b567832218
DIOR <diertojiev@yandex.ru>
2024-03-31 17:29:02 +0300

    Конверватор готов

--- script.js
+++ script.js
@@ -8,19 +8,25 @@ const numbers = {
     "M": 1000
 }
 
-let romanNum = 'M',
+let romanNum = 'X',
     arabicNum = 0;
 
-let romanNumArr = romanNum.split('')
-
-console.log(romanNumArr)
+let romanNumArr = romanNum.split('');
 
 romanNumArr.forEach((item, index, arr) => {
     for (let key in numbers) {
-        if (item == key) {
-            arabicNum += numbers[key]
+        if (item === key) {
+            arr[arr.indexOf(item)] = numbers[key];
         }
     }
 })
+
+for (let i = 0; i < romanNumArr.length; i++) {
+    if (romanNumArr[i] < romanNumArr[i + 1]) {
+        arabicNum -= romanNumArr[i]
+    } else {
+        arabicNum += romanNumArr[i]
+    }
+}
 
 console.log(arabicNum)
